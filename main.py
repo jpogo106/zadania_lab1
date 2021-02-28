4179d0ad3ee50619134d5f875d0601a21bc21bb9
123 <puste54@wp.pl>
2021-02-28 12:39:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+#zad1
+# a = 'napis'
+# b = 'napis1'
+# print(a,b)
+# c = 1
+# d = 5
+# print(c,d)
+# e = 5.67
+# f = 2.4
+# print(e,f)
+# g = 9+1j
+# h = 6+3j
+# print(g,h)
+
+
+#zad2
+# def dodawanie(x,y):
+#     return x+y
+# def odejmowanie(x,y):
+#     return x-y
+# def mnozenie(x,y):
+#     return x*y
+# def dzielenie(x,y):
+#     return x/y
+#
+# print("wybierz operacje:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie")
+#
+# while True:
+#     wybor = input("podaj numer operacji(1/2/3/4): ")
+#
+#     if wybor in ('1', '2', '3', '4'):
+#         liczba1 = float(input("podaj pierwsza liczbe: "))
+#         liczba2 = float(input("podaj druga liczbe: "))
+#
+#         if wybor == '1':
+#             print(liczba1, "+", liczba2, "=", dodawanie(liczba1, liczba2))
+#
+#         elif wybor == '2':
+#             print(liczba1, "-", liczba2, "=", odejmowanie(liczba1, liczba2))
+#
+#         elif wybor == '3':
+#             print(liczba1, "*", liczba2, "=", mnozenie(liczba1, liczba2))
+#
+#         elif wybor == '4':
+#             print(liczba1, "/", liczba2, "=", dzielenie(liczba1, liczba2))
+#         break
+#     else:
+#         print("zla liczba")
+
+
+# #zad3
+# a,b = 1,7
+# a+=b
+# print(a)
+# a-=b
+# print(a)
+# a*=b
+# print(a)
+# a/=b
+# print(a)
+# a**=b
+# print(a)
+# a%=b
+# print
+
+
+
+#zad4
+# print(pow(e,10))
+# print(log(5+(sin(8))**2)**(1/6))
+# print(floor(3.55))
+# print(ceil(4.80))
+
+
+#zad.5
+# x="JAKUB"
+# y="POGORZELSKI"
+# print(x.capitalize() + " " + y.capitalize())
+
+
+#zad.6
+# cos="I'm turning up the volume when you speak Cause if my heart cant stop it, I find a way to block it, I go La la, la la la la la na na na na na La la na na, la la la la la na na na na na"
+# print(cos.count("la"))
+
+
+
+# zad.7
+# x='laptop'
+# print(x[0])
+# print(x[5])
+
+
+#zad.8
+# print(cos.split())
+
+
+# zad.9
+# int='siema'
+# float=5.3
+# z=456
+# hex=hex(z)
+# print(int,float,hex)
